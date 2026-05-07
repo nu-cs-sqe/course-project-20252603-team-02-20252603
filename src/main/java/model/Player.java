5f46c47824ff06d427b9deea793e9e959252695b
kkurbae <kaitlynwang96@gmail.com>
2026-05-07 12:42:07 -0500

    IsAlive_OnAlivePlayer_ExpectTrue (T1) passes

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,7 @@
 package model;
 
 public class Player {
+    public boolean isAlive() {
+        return true;
+    }
 }
--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -1,4 +1,13 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PlayerTest {
+    @Test
+    public void IsAlive_OnAlivePlayer_ExpectTrue() {
+        Player player = new Player();
+        assertTrue(player.isAlive());
+    }
 }
