67304e4a4bec6e2195227d9567f9ddeb6cd9df22
Kevin Han <kevinhan@Kevins-MacBook-Air-10.local>
2026-05-13 18:31:16 -0500

    Passed card equals tests

--- docs/bva/Card.md
+++ docs/bva/Card.md
@@ -22,3 +22,24 @@
 - **TC2: Invalid Initialization** ( :white_check_mark: )
   - **Input**: null
   - **Output**: IllegalArgumentException
+
+### Method under test: `equals()`
+#### 1.
+- Input: Card, Card
+- Output: Equal or not
+#### 2.
+- Input: Cases, Cases
+- Output: Boolean
+#### 3.
+- Input:
+  - EXPLODING_KITTEN, DEFUSE, ATTACK, SKIP, FAVOR, SHUFFLE, NOPE, SEE_THE_FUTURE, TACOCAT, HAIRY_POTATO_CAT, RAINBOW_RALPHING_CAT, BEARD_CAT, CATTERMELON,
+  - EXPLODING_KITTEN, DEFUSE, ATTACK, SKIP, FAVOR, SHUFFLE, NOPE, SEE_THE_FUTURE, TACOCAT, HAIRY_POTATO_CAT, RAINBOW_RALPHING_CAT, BEARD_CAT, CATTERMELON,
+- Output: True, False
+#### 4.
+- **TC3: Card Equals Itself** ( :white_check_mark: )
+  - Inputs: Cards w/ matching CardTypes
+  - Outputs: True
+
+- **TC4: Card Doesn't Equal Another Card** ( :white_check_mark: )
+  - Inputs: Cards w/o matching CardTypes
+  - Outputs: False
--- src/test/java/model/CardTest.java
+++ src/test/java/model/CardTest.java
@@ -3,6 +3,7 @@ package model;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,4 +22,35 @@ public class CardTest {
             new Card(null);
         });
     }
+
+    @ParameterizedTest
+    @EnumSource(CardType.class)
+    void testEquals_SameCardType_ReturnsTrue(CardType type) {
+        Card card1 = new Card(type);
+        Card card2 = new Card(type);
+
+        assertTrue(card1.equals(card2));
+    }
+
+//    @ParameterizedTest
+//    @CsvSource({
+//            "DEFUSE, ATTACK",
+//            "ATTACK, SKIP",
+//            "SKIP, FAVOR",
+//            "FAVOR, SHUFFLE",
+//            "SHUFFLE, NOPE",
+//            "NOPE, SEE_THE_FUTURE",
+//            "SEE_THE_FUTURE, TACOCAT",
+//            "TACOCAT, HAIRY_POTATO_CAT",
+//            "HAIRY_POTATO_CAT, RAINBOW_RALPHING_CAT",
+//            "RAINBOW_RALPHING_CAT, BEARD_CAT",
+//            "BEARD_CAT, CATTERMELON",
+//            "CATTERMELON, DEFUSE"
+//    })
+//    void testEquals_DifferentCardTypes_ReturnsFalse(CardType type1, CardType type2) {
+//        Card card1 = new Card(type1);
+//        Card card2 = new Card(type2);
+//
+//        assertFalse(card1.equals(card2));
+//    }
 }
