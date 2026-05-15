97c0e6642f3cffe625f6462b8eb01844b77beabe
AnaGRamirez <araan.ramirez@gmail.com>
2026-05-14 19:19:12 -0500

    Fix checkstyle for card class and tests

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,31 +1,34 @@
 package model;
 
+import java.util.Objects;
+
 public class Card {
-    private final CardType type;
+  private final CardType type;
 
-    public Card(CardType type) {
-        if (type == null) {
-            throw new IllegalArgumentException("CardType cannot be null");
-        }
+  public Card(CardType type) {
+    this.type = Objects.requireNonNull(type, "CardType cannot be null");
+  }
 
-        this.type = type;
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-
-        Card card = (Card) obj;
-        return type == card.type;
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
     }
 
-    public CardType getType() {
-        return type;
-    }
+    Card card = (Card) obj;
+    return type == card.type;
+  }
+
+  @Override
+  public int hashCode() {
+    return type.hashCode();
+  }
+
+  public CardType getType() {
+    return type;
+  }
 }
