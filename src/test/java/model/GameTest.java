51b541cf38c69aaf112400c33ee87f7751b9fe6e
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 14:20:20 -0500

    add unit test moveToNextPlayerNextHasZeroTurnsOwed

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -838,4 +838,32 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
+  @Test
+  public void moveToNextPlayerNextHasZeroTurnsOwed() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer2.getTurnsOwed()).andReturn(0).once();
+    mockPlayer2.addTurn();
+    expect(mockPlayer2.getTurnsOwed()).andReturn(1).once();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.moveToNextPlayer();
+
+    assertEquals(1, game.getCurrentPlayerIndex());
+    assertEquals(1, game.getPlayers().get(1).getTurnsOwed());
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 }
