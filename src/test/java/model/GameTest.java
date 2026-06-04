036544c41aacde2203627480f3356b651d2685e5
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 15:01:11 -0500

    add unit test drawCardLastCardInDeck

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -973,4 +973,37 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockCard);
   }
+  @Test
+  public void drawCardLastCardInDeck() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+    Card mockCard = createMock(Card.class);
+
+    expect(mockDeck.drawCard()).andReturn(mockCard).once();
+    expect(mockCard.getType()).andReturn(CardType.SKIP).anyTimes();
+    mockPlayer1.addCard(mockCard);
+    mockPlayer1.removeTurn();
+    expect(mockPlayer1.getTurnsOwed()).andReturn(0).once();
+    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer2.getTurnsOwed()).andReturn(1).once();
+    expect(mockDeck.getDeck()).andReturn(List.of()).once();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockCard);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.drawCard();
+
+    assertEquals(1, game.getCurrentPlayerIndex());
+    assertTrue(game.getDrawPile().isEmpty());
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockCard);
+  }
 }
