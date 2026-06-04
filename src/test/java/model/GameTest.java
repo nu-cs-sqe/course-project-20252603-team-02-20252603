ebed5c5d186cc1869c59c5e76f0eea1c1c4f0287
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 02:11:34 -0500

    Add unit test for startGame with valid player count

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -23,4 +23,29 @@ public class GameTest {
   private static final int TOO_FEW_PLAYERS = 2;
   private static final int TOO_MANY_PLAYERS = 6;
 
+
+  @Test
+  public void startGameValidPlayerCount() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Player mockPlayer4 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockPlayer4, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3, mockPlayer4),
+        mockDeck,
+        new Random(42)
+    );
+
+    game.startGame();
+
+    assertTrue(game.isGameLaunched());
+    assertFalse(game.isGameOver());
+    assertEquals(0, game.getCurrentPlayerIndex());
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockPlayer4, mockDeck);
+  }
 }
