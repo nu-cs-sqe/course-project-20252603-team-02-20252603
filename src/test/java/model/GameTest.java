520a25985e309f6e9385a8862692118d2c1f2ce3
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 04:07:09 -0500

    Add unit test run game and game becomes over in middle of round

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -465,4 +465,36 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
+  @Test
+  public void runGameWhenGameBecomesOverDuringTurn() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+    Card mockCard = createMock(Card.class);
+
+    expect(mockCard.getType()).andReturn(CardType.EXPLODING_KITTEN).anyTimes();
+    expect(mockDeck.drawCard()).andReturn(mockCard).once();
+    expect(mockPlayer1.hasDefuse()).andReturn(false).once();
+    mockPlayer1.die();
+    expect(mockPlayer1.isAlive()).andReturn(false).anyTimes();
+    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer3.isAlive()).andReturn(false).anyTimes();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockCard);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.runGame();
+
+    assertTrue(game.isGameOver());
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockCard);
+  }
 }
