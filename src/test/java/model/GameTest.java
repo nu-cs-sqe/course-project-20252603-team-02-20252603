a19c94638d358832d9e82cc3f454d8e04669bf4f
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 14:09:25 -0500

    add unit test getNextActivePlayerOnlyOneAliveLeft ends game

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -732,4 +732,29 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
+  @Test
+  public void getNextActivePlayerOnlyOneAliveLeft() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    expect(mockPlayer2.isAlive()).andReturn(false).anyTimes();
+    expect(mockPlayer3.isAlive()).andReturn(false).anyTimes();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    assertNull(game.getNextActivePlayer());
+    assertTrue(game.isGameOver());
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
+
 }
