67c8ac4e717e10b6919c364f03d76d84dee26abb
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 23:10:04 -0500

    add unit test playBubonicPlagueOnePlayerHasNoCards

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1636,6 +1636,7 @@ public class GameTest {
     assertEquals(0, game.getCurrentPlayerIndex());
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
   @Test
   public void playBubonicPlagueAllOtherPlayersHaveCards() {
     Player mockPlayer1 = createMock(Player.class);
@@ -1647,7 +1648,6 @@ public class GameTest {
     Card card2 = new Card(CardType.SKIP);
     Card card3 = new Card(CardType.ATTACK);
 
-    // mockPlayer2: alive and has one card
     expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
     expect(mockPlayer2.getHand()).andReturn(List.of(card2)).times(2);
     expect(mockRandom.nextInt(1)).andReturn(0).once();
@@ -1656,7 +1656,6 @@ public class GameTest {
     expect(mockRandom.nextInt(1)).andReturn(0).once();
     mockDeck.addToDrawPile(card2, 0);
 
-    // mockPlayer3: alive and has one card
     expect(mockPlayer3.isAlive()).andReturn(true).anyTimes();
     expect(mockPlayer3.getHand()).andReturn(List.of(card3)).times(2);
     expect(mockRandom.nextInt(1)).andReturn(0).once();
@@ -1680,4 +1679,42 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockRandom);
   }
+
+  @Test
+  public void playBubonicPlagueOnePlayerHasNoCards() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+    Random mockRandom = createMock(Random.class);
+
+    Card card3 = new Card(CardType.ATTACK);
+
+    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer2.getHand()).andReturn(List.of()).once();
+
+    expect(mockPlayer3.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer3.getHand()).andReturn(List.of(card3)).times(2);
+    expect(mockRandom.nextInt(1)).andReturn(0).once();
+    mockPlayer3.removeCard(card3);
+    expect(mockDeck.getDeck()).andReturn(List.of()).once();
+    expect(mockRandom.nextInt(1)).andReturn(0).once();
+    mockDeck.addToDrawPile(card3, 0);
+
+    mockDeck.shuffle();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockRandom);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        mockRandom
+    );
+
+    game.startGame();
+    game.playBubonicPlague();
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck, mockRandom);
+  }
+
 }
