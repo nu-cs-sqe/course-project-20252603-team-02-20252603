06b2c452661ecff3f8d64f564ce508162e6d0004
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 21:47:25 -0500

    add unit test playSkipCardTwoOwedRemainsCurrentPlayer

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1609,4 +1609,31 @@ public class GameTest {
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
 
+  @Test
+  public void playSkipCardTwoOwedRemainsCurrentPlayer() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card skipCard = new Card(CardType.SKIP);
+    mockPlayer1.removeCard(skipCard);
+    mockDeck.discardCard(skipCard);
+    expectLastCall().once();
+    mockPlayer1.removeTurn();
+    expect(mockPlayer1.getTurnsOwed()).andReturn(1).once();
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.playCard(skipCard);
+
+    assertEquals(0, game.getCurrentPlayerIndex());
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 }
