aca5524d5056ca15497e9be330ace4d913f13475
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 23:31:00 -0500

    add unit defusePositionEqualToSizeInsertsAtBottom

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -2016,4 +2016,29 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
+  @Test
+  public void defusePositionEqualToSizeInsertsAtBottom() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card topCard = new Card(CardType.SKIP);
+    Card explodingKitten = new Card(CardType.EXPLODING_KITTEN);
+    expect(mockDeck.getDeck()).andReturn(List.of(topCard)).once();
+    mockDeck.addToDrawPile(explodingKitten, 1);
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.defuse(1);
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 }
