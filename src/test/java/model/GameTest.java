ad6908989739439f7c4e2ec7bfd2502b2ac5f771
AnaGRamirez <araan.ramirez@gmail.com>
2026-06-04 21:46:30 -0500

    add unit test playSkipCardOneOwedAdvancesTurn

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -1578,4 +1578,35 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
+  @Test
+  public void playSkipCardOneOwedAdvancesTurn() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card skipCard = new Card(CardType.SKIP);
+    mockPlayer1.removeCard(skipCard);
+    mockDeck.discardCard(skipCard);
+    expectLastCall().once();
+    mockPlayer1.removeTurn();
+    expect(mockPlayer1.getTurnsOwed()).andReturn(0).once();
+    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer2.getTurnsOwed()).andReturn(1).once();
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.playCard(skipCard);
+
+    assertEquals(1, game.getCurrentPlayerIndex());
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
+
 }
