b11f858dd667a45f951d51278296a685cef3626c
Clarissa Shieh <clarissamshieh@gmail.com>
2026-06-06 02:27:36 -0500

    create unit test playNosyOnSelfThrowException

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -3003,4 +3003,29 @@ public class GameTest {
 
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
+
+  @Test
+  public void playNosyOnSelfThrowException() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card nosyCard = new Card(CardType.NOSY);
+    expect(mockPlayer1.getHand()).andReturn(List.of(nosyCard)).anyTimes();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+
+    assertThrows(IllegalArgumentException.class, () -> game.playNosy(0));
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 }
