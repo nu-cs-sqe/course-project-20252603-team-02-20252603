cf1b7548dd365c1b766e5e2a7a2a37aaf0b374e0
Kevin Han <kevinhan@Kevins-MacBook-Air-10.local>
2026-06-11 14:06:08 -0700

    Passing playCatCardsThreeMatchingCatsTargetLacksNamedCardNothingTransferred

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -4827,40 +4827,34 @@ public class GameTest {
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
 
-//  @Test
-//  public void playCatCardsThreeMatchingCatsTargetLacksNamedCardNothingTransferred() {
-//    Player mockPlayer1 = createMock(Player.class);
-//    Player mockPlayer2 = createMock(Player.class);
-//    Player mockPlayer3 = createMock(Player.class);
-//    Deck mockDeck = createMock(Deck.class);
-//
-//    Card cat1 = new Card(CardType.TACOCAT);
-//    Card cat2 = new Card(CardType.TACOCAT);
-//    Card cat3 = new Card(CardType.TACOCAT);
-//
-//    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
-//    expect(mockPlayer1.getHand()).andReturn(List.of(cat1, cat2, cat3)).anyTimes();
-//    mockPlayer1.removeCard(cat1);
-//    mockPlayer1.removeCard(cat2);
-//    mockPlayer1.removeCard(cat3);
-//    mockDeck.discardCard(cat1);
-//    mockDeck.discardCard(cat2);
-//    mockDeck.discardCard(cat3);
-//    expect(mockPlayer2.getHand()).andReturn(List.of(new Card(CardType.ATTACK))).anyTimes();
-//
-//    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
-//
-//    Game game = new Game(
-//        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
-//        mockDeck,
-//        new Random(RANDOM_SEED)
-//    );
-//
-//    game.startGame();
-//    game.playCatCards(List.of(cat1, cat2, cat3), mockPlayer2, CardType.FAVOR);
-//
-//    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
-//  }
+  @Test
+  public void playCatCardsThreeMatchingCatsTargetLacksNamedCardNothingTransferred() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card cat1 = new Card(CardType.TACOCAT);
+    Card cat2 = new Card(CardType.TACOCAT);
+    Card cat3 = new Card(CardType.TACOCAT);
+
+    expect(mockPlayer2.isAlive()).andReturn(true).anyTimes();
+    expect(mockPlayer1.getHand()).andReturn(List.of(cat1, cat2, cat3)).anyTimes();
+    expect(mockPlayer2.getHand()).andReturn(List.of(new Card(CardType.ATTACK))).anyTimes();
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.playCatCards(List.of(cat1, cat2, cat3), mockPlayer2, CardType.FAVOR);
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 
 //  @Test
 //  public void playCatCardsFiveDifferentCatsNamedCardInDiscardTransfersCard() {
