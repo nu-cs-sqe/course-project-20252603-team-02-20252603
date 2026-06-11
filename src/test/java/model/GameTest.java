4abbfb3284a1e86794b87ecf0010c53790246b86
Kevin Han <kevinhan@Kevins-MacBook-Air-10.local>
2026-06-11 14:06:56 -0700

    Passing playCatCardsFiveDifferentCatsNamedCardInDiscardTransfersCard

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -4856,48 +4856,38 @@ public class GameTest {
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
 
-//  @Test
-//  public void playCatCardsFiveDifferentCatsNamedCardInDiscardTransfersCard() {
-//    Player mockPlayer1 = createMock(Player.class);
-//    Player mockPlayer2 = createMock(Player.class);
-//    Player mockPlayer3 = createMock(Player.class);
-//    Deck mockDeck = createMock(Deck.class);
-//
-//    Card cat1 = new Card(CardType.TACOCAT);
-//    Card cat2 = new Card(CardType.HAIRY_POTATO_CAT);
-//    Card cat3 = new Card(CardType.RAINBOW_RALPHING_CAT);
-//    Card cat4 = new Card(CardType.BEARD_CAT);
-//    Card cat5 = new Card(CardType.CATTERMELON);
-//    Card favor = new Card(CardType.FAVOR);
-//
-//    expect(mockPlayer1.getHand())
-//        .andReturn(List.of(cat1, cat2, cat3, cat4, cat5)).anyTimes();
-//    expect(mockDeck.takeFromDiscard(CardType.FAVOR)).andReturn(favor).once();
-//    mockPlayer1.removeCard(cat1);
-//    mockPlayer1.removeCard(cat2);
-//    mockPlayer1.removeCard(cat3);
-//    mockPlayer1.removeCard(cat4);
-//    mockPlayer1.removeCard(cat5);
-//    mockDeck.discardCard(cat1);
-//    mockDeck.discardCard(cat2);
-//    mockDeck.discardCard(cat3);
-//    mockDeck.discardCard(cat4);
-//    mockDeck.discardCard(cat5);
-//    mockPlayer1.addCard(favor);
-//
-//    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
-//
-//    Game game = new Game(
-//        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
-//        mockDeck,
-//        new Random(RANDOM_SEED)
-//    );
-//
-//    game.startGame();
-//    game.playCatCards(List.of(cat1, cat2, cat3, cat4, cat5), null, CardType.FAVOR);
-//
-//    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
-//  }
+  @Test
+  public void playCatCardsFiveDifferentCatsNamedCardInDiscardTransfersCard() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card cat1 = new Card(CardType.TACOCAT);
+    Card cat2 = new Card(CardType.HAIRY_POTATO_CAT);
+    Card cat3 = new Card(CardType.RAINBOW_RALPHING_CAT);
+    Card cat4 = new Card(CardType.BEARD_CAT);
+    Card cat5 = new Card(CardType.CATTERMELON);
+    Card favor = new Card(CardType.FAVOR);
+
+    expect(mockPlayer1.getHand())
+        .andReturn(List.of(cat1, cat2, cat3, cat4, cat5)).anyTimes();
+    expect(mockDeck.takeFromDiscard(CardType.FAVOR)).andReturn(favor).once();
+    mockPlayer1.addCard(favor);
+
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.playCatCards(List.of(cat1, cat2, cat3, cat4, cat5), null, CardType.FAVOR);
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 
 //  @Test
 //  public void playCatCardsFiveDifferentCatsNamedCardNotInDiscardThrowsIllegalStateException() {
