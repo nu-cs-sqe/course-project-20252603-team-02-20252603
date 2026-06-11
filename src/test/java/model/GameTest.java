faea6ad3baac7a7888615275ae1b1fb03fd5abd1
Kevin Han <kevinhan@Kevins-MacBook-Air-10.local>
2026-06-11 15:12:50 -0700

    Passing defuseMiddlePositionInsertsAtMiddle

--- src/test/java/model/GameTest.java
+++ src/test/java/model/GameTest.java
@@ -2236,31 +2236,34 @@ public class GameTest {
     verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
   }
 
-//  @Test
-//  public void defuseMiddlePositionInsertsAtMiddle() {
-//    Player mockPlayer1 = createMock(Player.class);
-//    Player mockPlayer2 = createMock(Player.class);
-//    Player mockPlayer3 = createMock(Player.class);
-//    Deck mockDeck = createMock(Deck.class);
-//
-//    Card topCard = new Card(CardType.SKIP);
-//    Card bottomCard = new Card(CardType.ATTACK);
-//    Card explodingKitten = new Card(CardType.EXPLODING_KITTEN);
-//    expect(mockDeck.getDeck()).andReturn(List.of(topCard, bottomCard)).once();
-//    mockDeck.addToDrawPile(explodingKitten, 1);
-//    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
-//
-//    Game game = new Game(
-//        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
-//        mockDeck,
-//        new Random(RANDOM_SEED)
-//    );
-//
-//    game.startGame();
-//    game.defuse(1);
-//
-//    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
-//  }
+  @Test
+  public void defuseMiddlePositionInsertsAtMiddle() {
+    Player mockPlayer1 = createMock(Player.class);
+    Player mockPlayer2 = createMock(Player.class);
+    Player mockPlayer3 = createMock(Player.class);
+    Deck mockDeck = createMock(Deck.class);
+
+    Card topCard = new Card(CardType.SKIP);
+    Card bottomCard = new Card(CardType.ATTACK);
+    Card explodingKitten = new Card(CardType.EXPLODING_KITTEN);
+    expect(mockPlayer1.hasDefuse()).andReturn(true).once();
+    expect(mockDeck.getDeck()).andReturn(List.of(topCard, bottomCard)).once();
+    mockPlayer1.removeCard(new Card(CardType.DEFUSE));
+    mockDeck.discardCard(new Card(CardType.DEFUSE));
+    mockDeck.addToDrawPile(explodingKitten, 1);
+    replay(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+
+    Game game = new Game(
+        List.of(mockPlayer1, mockPlayer2, mockPlayer3),
+        mockDeck,
+        new Random(RANDOM_SEED)
+    );
+
+    game.startGame();
+    game.defuse(1);
+
+    verify(mockPlayer1, mockPlayer2, mockPlayer3, mockDeck);
+  }
 
   @Test
   public void playCardSwapTopBottomEmptyDeck() {
