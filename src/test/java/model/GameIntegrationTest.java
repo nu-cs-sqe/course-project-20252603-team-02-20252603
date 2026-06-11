0ccc8f45a8c9acab34bcbfd21670db583e959fd6
Kevin Han <kevinhan@Kevins-MacBook-Air-10.local>
2026-06-11 14:39:21 -0700

    Passing playCatCardsFiveDifferentCatsNamedCardNotInDiscardThrowsIllegalArgumentException

--- src/test/java/model/GameIntegrationTest.java
+++ src/test/java/model/GameIntegrationTest.java
@@ -3154,29 +3154,31 @@ public class GameIntegrationTest {
     assertFalse(game.getDiscardPile().contains(favor));
   }
 
-//  @Test
-//  public void playCatCardsFiveDifferentCatsNamedCardNotInDiscardThrowsIllegalArgumentException() {
-//    Game game = new Game(MIN_PLAYERS, new Random(RANDOM_SEED));
-//    game.startGame();
-//    Player currentPlayer = game.getCurrentPlayer();
-//
-//    Card cat1 = new Card(CardType.TACOCAT);
-//    Card cat2 = new Card(CardType.HAIRY_POTATO_CAT);
-//    Card cat3 = new Card(CardType.RAINBOW_RALPHING_CAT);
-//    Card cat4 = new Card(CardType.BEARD_CAT);
-//    Card cat5 = new Card(CardType.CATTERMELON);
-//
-//    currentPlayer.addCard(cat1);
-//    currentPlayer.addCard(cat2);
-//    currentPlayer.addCard(cat3);
-//    currentPlayer.addCard(cat4);
-//    currentPlayer.addCard(cat5);
-//
-//    Exception e = assertThrows(IllegalStateException.class, () ->
-//        game.playCatCards(List.of(cat1, cat2, cat3, cat4, cat5), null, CardType.FAVOR));
-//
-//    assertEquals("discard pile is empty", e.getMessage());
-//  }
+  @Test
+  public void playCatCardsFiveDifferentCatsNamedCardNotInDiscardThrowsIllegalArgumentException() {
+    Game game = new Game(MIN_PLAYERS, new Random(RANDOM_SEED));
+    game.startGame();
+    Player currentPlayer = game.getCurrentPlayer();
+
+    Card cat1 = new Card(CardType.TACOCAT);
+    Card cat2 = new Card(CardType.HAIRY_POTATO_CAT);
+    Card cat3 = new Card(CardType.RAINBOW_RALPHING_CAT);
+    Card cat4 = new Card(CardType.BEARD_CAT);
+    Card cat5 = new Card(CardType.CATTERMELON);
+
+    currentPlayer.addCard(cat1);
+    currentPlayer.addCard(cat2);
+    currentPlayer.addCard(cat3);
+    currentPlayer.addCard(cat4);
+    currentPlayer.addCard(cat5);
+
+    Exception e = assertThrows(IllegalArgumentException.class, () -> {
+      game.playCard(List.of(cat1, cat2, cat3, cat4, cat5), null, CardType.FAVOR);
+      game.resolvePendingAction();
+    });
+
+    assertEquals("card type not in discard pile", e.getMessage());
+  }
 
   @Test
   public void playFavorTargetHasTwoCardsGivenMatchesOneTransfersCorrectCard() {
