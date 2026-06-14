5080f5d363c17a52b5c77e6a72ccbd7b51ef1945
Kevin Han <146395705+kevinhan384@users.noreply.github.com>
2026-06-13 22:52:44 -0500

    Fixing checkstyle indents

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -3,8 +3,8 @@ package ui;
 import java.util.Locale;
 
 public class Main {
-    public static void main(String[] args) {
-        Locale locale = args.length > 0 ? Locale.forLanguageTag(args[0]) : Locale.getDefault();
-        new ConsoleUI(locale).start();
-    }
+  public static void main(String[] args) {
+      Locale locale = args.length > 0 ? Locale.forLanguageTag(args[0]) : Locale.getDefault();
+      new ConsoleUI(locale).start();
+  }
 }
